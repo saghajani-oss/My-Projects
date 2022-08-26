36bff7b4846d55fd6fbbbbe5c712046701b372f6
saghajani-oss <112084533+saghajani-oss@users.noreply.github.com>
2022-08-26 19:47:54 +0430

    Weather App- Week 5

--- js/script.js
+++ js/script.js
@@ -1,1 +1,44 @@
-alert("hello");
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let currentDay = days[now.getDay()];
+let currentHour = ("0" + now.getHours()).substr(-2);
+let currentMinute = ("0" + now.getMinutes()).substr(-2);
+
+let currentTime = document.querySelector("#date");
+currentTime.innerHTML = `${currentDay} ${currentHour}:${currentMinute}`;
+
+function weatherReport(response) {
+  console.log(response);
+  let temp = Math.round(response.data.main.temp);
+  let temperature = document.querySelector(".degree");
+  temperature.innerHTML = `${temp} °C`;
+
+  let cityName = response.data.name;
+  let CurrentCity = document.querySelector("#current-city");
+  CurrentCity.innerHTML = cityName;
+
+  let currentWind = document.querySelector(".row-two");
+  currentWind.innerHTML = `Wind: ${Math.round(response.data.wind.speed)}km/h`;
+}
+
+function enterCityName(event) {
+  event.preventDefault();
+  let apiKey = "3bad7bd4137a5ff9f5c84125992a313a";
+  let cityInput = document.querySelector("#city-name");
+  let city = cityInput.value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+  console.log(apiUrl);
+
+  axios.get(apiUrl).then(weatherReport);
+}
+
+let citySearch = document.querySelector("#city-search");
+citySearch.addEventListener("submit", enterCityName);
